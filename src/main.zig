abe73771fd824224e16934d7344d8be3add353a1
yo-reign <danielaguiar0006@gmail.com>
2025-11-15 21:00:01 -0800

    working starting point example

--- src/main.zig
+++ src/main.zig
@@ -1,6 +1,49 @@
 const std = @import("std");
 const zimg = @import("zigimg");
 
+const white = zimg.Colors(zimg.color.Bgra32).White;
+const green = zimg.Colors(zimg.color.Bgra32).Green;
+const red = zimg.Colors(zimg.color.Bgra32).Red;
+const blue = zimg.Colors(zimg.color.Bgra32).Blue;
+const yellow = zimg.Colors(zimg.color.Bgra32).Yellow;
+
 pub fn main() !void {
-    std.debug.print("All your {s} are belong to us.\n", .{"codebase"});
+    var gpa = std.heap.GeneralPurposeAllocator(.{}){};
+    defer {
+        const result = gpa.deinit();
+        if (result == .leak) {
+            std.debug.print("leaked memory\n", .{});
+        }
+    }
+    const allocator = gpa.allocator();
+
+    const width: usize = 64;
+    const height: usize = 64;
+
+    var framebuffer = try zimg.Image.create(allocator, width, height, .bgra32);
+    defer framebuffer.deinit(allocator);
+
+    const ax = 7;
+    const ay = 3;
+    const bx = 12;
+    const by = 37;
+    const cx = 62;
+    const cy = 53;
+
+    framebuffer.pixels.bgra32[atPixel(width, ax, ay)] = white;
+    framebuffer.pixels.bgra32[atPixel(width, bx, by)] = white;
+    framebuffer.pixels.bgra32[atPixel(width, cx, cy)] = white;
+
+    var write_buffer: [zimg.io.DEFAULT_BUFFER_SIZE]u8 = undefined;
+
+    // OUTPUT TGA FILE
+    // try tga_framebuffer.writeToFilePath(allocator, "framebuffer.tga", write_buffer[0..], .{ .tga = .{ .author_name = "yo-reign" } });
+
+    // OUTPUT AS PNG INSTEAD
+    try framebuffer.convert(allocator, .rgba32);
+    try framebuffer.writeToFilePath(allocator, "framebuffer.png", write_buffer[0..], .{ .png = .{ .filter_choice = .{ .specified = .none } } });
+}
+
+fn atPixel(framebuffer_width: usize, x: usize, y: usize) usize {
+    return ((y * framebuffer_width) + x);
 }
